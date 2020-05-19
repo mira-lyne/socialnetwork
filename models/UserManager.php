6de3a505a5d579dff98936ff85ad718c970b8531
Mira <amira.farreh@gmail.com>
2020-05-19 16:56:51 +0200

    commit

--- models/UserManager.php
+++ models/UserManager.php
@@ -18,5 +18,13 @@ function GetAllUsers()
 
 function GetUserIdFromUserAndPassword($username, $password)
 {
-  return $username;
+  global $PDO;
+  $res = $PDO->query("SELECT * FROM user WHERE nickname = '$username' AND password = 'password'");
+  $users = $res->fetchAll();
+  if (count($users) == 1) {
+    $id = $users[0]['id'];
+    return $id;
+  } else {
+    return -1;
+  }
 }
